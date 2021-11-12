9008207332be9b81c111c46b39a23d18b72d9f99
mdemetriou24 <mdemetriou@oakland.edu>
2021-11-12 11:21:55 -0500

    big update, JS code

---
+++ JS/JScode1.js
@@ -0,0 +1,33 @@
+
+function textTyped(){
+    var inputText = document.getElementById("itemTxt").value;
+    var submitBtn = document.getElementById("submit");  
+    if(inputText.length == 0) {
+        submitBtn.disabled = true;
+    }
+    else {
+        submitBtn.disabled = false;
+    }
+ }
+ function addItem() {  
+    var inputText = document.getElementById("itemTxt");  
+    var submitBtn = document.getElementById("submit");  
+    var nextBlock = document.getElementById("secondBlock");  
+    var newPara = document.createElement("p");  
+    var date = new Date();
+    newPara.innerHTML = inputText.value + "<br>" + date.getDate() + "/" + (parseInt(date.getMonth())+1) + "/" + date.getFullYear();  
+    var deleteBtn = document.createElement("BUTTON");
+    deleteBtn.innerHTML = "DELETE ITEM";
+    deleteBtn.onclick = function() {
+        newPara.remove();
+    }
+    newPara.style.width = "500px";
+    deleteBtn.style.cssFloat = "right";  
+    newPara.appendChild(deleteBtn);
+    nextBlock.appendChild(newPara);      
+    nextBlock.style.visibility = "visible";  
+    inputText.value = "";
+    submitBtn.disabled = true;
+    return false;
+ }
+ 
