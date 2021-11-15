8d3c14adcef5cf76a6cf25459af906098eacee17
mdemetriou24 <mdemetriou@oakland.edu>
2021-11-14 22:42:44 -0500

    try to make a comment system

--- JS/JScode1.js
+++ JS/JScode1.js
@@ -30,4 +30,37 @@ function textTyped(){
     submitBtn.disabled = true;
     return false;
  }
+
+ function text(){
+    var inputText = document.getElementById("item").value;
+    var submitBtn = document.getElementById("sub");  
+    if(inputText.length == 0) {
+        submitBtn.disabled = true;
+    }
+    else {
+        submitBtn.disabled = false;
+    }
+ }
+
+ function add() {  
+    var inputText = document.getElementById("item");  
+    var submitBtn = document.getElementById("sub");  
+    var nextBlock = document.getElementById("second");  
+    var newPara = document.createElement("p");  
+    var date = new Date();
+    newPara.innerHTML = inputText.value + "<br>" + date.getDate() + "/" + (parseInt(date.getMonth())+1) + "/" + date.getFullYear();  
+    var deleteBtn = document.createElement("BUTTON");
+    deleteBtn.innerHTML = "DELETE ITEM";
+    deleteBtn.onclick = function() {
+        newPara.remove();
+    }
+    newPara.style.width = "500px";
+    deleteBtn.style.cssFloat = "right";  
+    newPara.appendChild(deleteBtn);
+    nextBlock.appendChild(newPara);      
+    nextBlock.style.visibility = "visible";  
+    inputText.value = "";
+    submitBtn.disabled = true;
+    return false;
+ }
  
